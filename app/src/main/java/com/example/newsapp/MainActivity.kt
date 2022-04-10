4c83136196d53c7dffc45e9c30bcf55814392e12
priya <priya@gmail.com>
2022-04-10 14:51:46 +0530

    Modifications are done

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,45 +1,21 @@
 package com.example.newsapp
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.news_data.Instance
-import kotlinx.coroutines.launch
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var newRecyclerView : RecyclerView
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val manager = LinearLayoutManager(this)
-
-        lifecycleScope.launch {
-            val instance = Instance.getNewsRepository()
-            val response = instance.getNewsHeadlines()
-            Toast.makeText(this@MainActivity, "${response.articles.size}",Toast.LENGTH_SHORT).show()
-            val adapter = recyclerViewAdapter(response.articles)
-
-            newRecyclerView = findViewById(R.id.recyclerView)
-            newRecyclerView.adapter = adapter
-            newRecyclerView.layoutManager = manager
-
-            adapter.setOnItemClickListener(object : recyclerViewAdapter.onItemClickListener{
-                override fun onItemClick(position: Int) {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(response.articles[position].url))
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    intent.setPackage("com.android.chrome")
-                    startActivity(intent)
-                }
-            })
+        if(savedInstanceState==null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment,NewsFragment.newInstance()).commit()
         }
+
     }
 }
--- app/src/main/java/com/example/newsapp/recyclerViewAdapter.kt
+++ app/src/main/java/com/example/newsapp/recyclerViewAdapter.kt
@@ -3,10 +3,12 @@ package com.example.newsapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.news_data.model.Articles
+import com.example.newsapp.databinding.ListItemBinding
 import com.squareup.picasso.Picasso
 
 class recyclerViewAdapter(private val news: ArrayList<Articles>):
@@ -21,14 +23,15 @@ class recyclerViewAdapter(private val news: ArrayList<Articles>):
         mListener = listener
     }
 
-    class MyViewHolder(itemView: View, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView) {
+    class MyViewHolder(itemView: ListItemBinding, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView.root) {
 
-        //        val image :ShapeableImageView = itemView.findViewById(R.id.image)
-        val view: TextView = itemView.findViewById(R.id.text)
-        val image: ImageView = itemView.findViewById(R.id.imageView)
+//        val view: TextView = itemView.findViewById(R.id.text)
+        val view: TextView = itemView.text
+        val image: ImageView = itemView.imageView
+
 
         init {
-            itemView.setOnClickListener {
+            itemView.root.setOnClickListener {
                 listener.onItemClick(adapterPosition)
             }
 
@@ -37,15 +40,20 @@ class recyclerViewAdapter(private val news: ArrayList<Articles>):
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
-        val itemView= LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent, false)
+        val itemView = ListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return MyViewHolder(itemView,mListener)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentItem = news[position]
+        val currentItem = news.get(position)
         holder.view.text=currentItem.title
         val picasso = Picasso.get()
         picasso.load(currentItem.urlToImage).into(holder.image)
+
+
+        //holder.gridview.text=currentItem.title
+        //val picasso = Picasso.get()
+        //picasso.load(currentItem.urlToImage).into(holder.image)
 
 
     }
